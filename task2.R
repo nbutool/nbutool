c849e85161954f9b18d4d1db552d6528c123fbb9
nbutool <97474727+nbutool@users.noreply.github.com>
2022-01-18 14:53:36 -0500

    Add files via upload

---
+++ task2.R
@@ -0,0 +1,6 @@
+## Task 2: Create a vector that holds the percentage of wetland cover at 3 
+## different locations, use a command to access the amount of cover at location 2
+
+wetland_cover <- c("location 1" = 50, "location 2" = 15, "location 3" = 35)
+
+wetland_cover[2]
